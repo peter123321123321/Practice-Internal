ead8142cc7cabc9016f5175643e0178db145b13c
peter123321123321 <98923575+peter123321123321@users.noreply.github.com>
2023-04-22 23:27:25 +1200

    Update WelcomeV3.py

--- WelcomeV3.py
+++ WelcomeV3.py
@@ -2,37 +2,27 @@ import easygui as eg
 
 
 def welcome():
-    guide = eg.buttonbox("Welcome to The Takeaway Shop\n"
-                         "What would you like to do today", "The Takeaway Shop",
-                         choices=["Add a combo", "Find an existing combo", "Delete a combo", "Show the menu", "Exit"])
-    function = {"Add a combo": add_combo,
-                "Find an existing combo": find_existing_combo,
-                "Delete a combo": delete_combo,
-                "Show the menu": show_menu,
-                "Exit": exit_program}
-    chosen_function = function.get(guide)
-    chosen_function()
+    while True:
+        guide = eg.buttonbox("Welcome to The Takeaway Shop\n"
+                             "What would you like to do today", "The Takeaway Shop",
+                             choices=["Add or remove a combo", "Find or show combos", "Exit"])
+        if guide == "Exit":
+            break
+        function = {"Add or remove a combo": add_or_remove,
+                    "Find or show combos": find_combo}
+        chosen_function = function.get(guide)
+        chosen_function()
 
 
-def add_combo():
+def add_or_remove():
     print("add_combo")
 
 
-def find_existing_combo():
+def find_combo():
     print("find_existing_combo")
-
-
-def delete_combo():
-    print("delete_combo")
-
-
-def show_menu():
-    print("show_menu")
-
-
-def exit_program():
-    print("exit_program")
 
 
 # Main Routine
 welcome()
+eg.msgbox("Thanks for using the Takeaway Shop", "Goodbye")
+
